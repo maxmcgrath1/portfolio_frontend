529ed3da156978e9eb2d057a98fc6a97c5e19553
maxmcgrath1 <tuc06096@temple.edu>
2022-05-03 17:56:58 -0400

    no dark viewer needed

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header(props) {
     const navStyle = {
         display: "flex",
         justifyContent: "space-around",
-        border: "3px solid black",
+        border: "3px solid white",
         padding: "8px",
         width: "90%",
         margin: "auto",
