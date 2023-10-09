10e18f8214fa73472f5fb7ce8686ab39346b5e87
maxmcgrath1 <tuc06096@temple.edu>
2023-10-09 18:40:48 -0400

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 function Home(props) {
     return <h1 className = "homeMessage">
+        <hr />
         Welcome to my portfolio! <br />
         My current work is under the projects tab! < br />
         There are 4 currently, but more will be available soon!
