bbfd990b2f51075052680ca4b0aacd6758ea1220
maxmcgrath1 <tuc06096@temple.edu>
2023-10-09 18:41:15 -0400

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ function Home(props) {
         Welcome to my portfolio! <br />
         My current work is under the projects tab! < br />
         There are 4 currently, but more will be available soon!
+        <hr />
     </h1>;
 }
 
